2053d671637264e4ce9a3fbb3f4b89ba61021255
melon <meloniusz@hotmail.com>
2022-04-30 20:01:19 +0800

    Fixing (plural s) with Django by using verbose_name

--- Shop/Products/models.py
+++ Shop/Products/models.py
@@ -1,8 +1,24 @@
 from django.db import models
 
 class Products(models.Model):
-    name = models.CharField(max_length=100)
+    name = models.CharField(max_length=60)
     description = models.TextField(blank=True)
-    price = models.DecimalField(max_digits=12, decimal_places=2)
+    price = models.DecimalField(max_digits=99999, decimal_places=2)
     def __str__(self):
         return self.name
+
+    class Meta:
+        verbose_name = "Product"
+        verbose_name_plural = "Products"
+
+    class Producer(models.Model):
+        def __str__(self):
+            return self.nazwa
+
+        name = models.CharField(max_length=60)
+        description = models.TextField(blank=True)
+
+        class Meta:
+            verbose_name = "Producer"
+            verbose_name_plural = "Producers"
+
