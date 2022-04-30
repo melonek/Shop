cae0ad20fd075a1edc4029fa8106ad448aeb593a
melon <meloniusz@hotmail.com>
2022-04-30 20:36:45 +0800

    adding relations between products and producers

--- Shop/Products/models.py
+++ Shop/Products/models.py
@@ -1,24 +1,25 @@
 from django.db import models
 
+class Producer(models.Model):
+    name = models.CharField(max_length=60)
+    description = models.TextField(blank=True)
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name = "Producer"
+        verbose_name_plural = "Producers"
+
 class Products(models.Model):
+    producer = models.ForeignKey(Producer, on_delete=models.CASCADE,null=True)
     name = models.CharField(max_length=60)
     description = models.TextField(blank=True)
     price = models.DecimalField(max_digits=99999, decimal_places=2)
+
     def __str__(self):
         return self.name
 
     class Meta:
         verbose_name = "Product"
         verbose_name_plural = "Products"
-
-    class Producer(models.Model):
-        def __str__(self):
-            return self.nazwa
-
-        name = models.CharField(max_length=60)
-        description = models.TextField(blank=True)
-
-        class Meta:
-            verbose_name = "Producer"
-            verbose_name_plural = "Producers"
-
